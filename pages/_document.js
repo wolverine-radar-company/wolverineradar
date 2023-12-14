f186badce31321b5172161d07e06c8e4ba534b86
Joseph Manni <141681636+dev-jmanni@users.noreply.github.com>
2023-12-14 15:33:50 -0500

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default function Document() {
         <link rel="apple-touch-icon" sizes="180x180" href="/wolverineradar/apple-touch-icon.png"/>
         <link rel="icon" type="image/png" sizes="32x32" href="/wolverineradar/favicon-32x32.png"/>
         <link rel="icon" type="image/png" sizes="16x16" href="/wolverineradar/favicon-16x16.png"/>
-        <link rel="manifest" href="../site.webmanifest"/>
+        <link rel="manifest" href="/wolverineradar/site.webmanifest"/>
         <meta name="msapplication-TileColor" content="#da532c"/>
         <meta name="theme-color" content="#ffffff"/>
       </Head>
